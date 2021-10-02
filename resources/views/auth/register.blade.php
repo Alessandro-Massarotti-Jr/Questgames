e80dbc0c0d0adb222df75ddf74ff3f86f260ad19
Hydeleto <alemassajr@hotmail.com>
2021-10-02 16:42:32 -0300

    testei algumas coisas no CSS do Login e Cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,23 +11,23 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <div class="cxglobal">
-            <x-jet-label for="name" value="{{ __('Name') }}" />
-            <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+        <div class="label">
+            <label for="name" value="{{ __('Name') }}">Nome</label>
+            <input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
         </div>
 
-        <div class="cxglobal">
-            <x-jet-label for="email" value="{{ __('Email') }}" />
-            <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+        <div class="label">
+            <label for="email" value="{{ __('Email') }}" >Email</label>
+            <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
         </div>
 
-        <div class="cxglobal">
-            <x-jet-label for="password" value="{{ __('Password') }}" />
-            <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+        <div class="label">
+            <label for="password" value="{{ __('Password') }}" >Senha</label>
+            <input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
         </div>
 
-        <div class="cxglobal">
-            <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+        <div class="label">
+            <label for="password_confirmation" value="{{ __('Confirm Password') }}" >Confirmar senha</label>
             <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
         </div>
 
