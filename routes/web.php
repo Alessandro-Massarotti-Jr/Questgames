6bdc3db36c58857f1731c347852247bafce02629
Hydeleto <alemassajr@hotmail.com>
2021-10-21 10:43:13 -0300

    comit adm

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/adm', function () {
+    return view('adm');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
