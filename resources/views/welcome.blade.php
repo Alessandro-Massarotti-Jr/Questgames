fe03612027435d9032e5169ee91de50a363bd619
Hydeleto <alemassajr@hotmail.com>
2021-10-30 16:29:46 -0300

    inicio slideshow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,96 +8,113 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 
-    <!-- Slideshow container -->
-    <div class="slideshow-container">
+    <div class="slideshow">
+        <div class="slide">
+            <input type="radio" name="radio-btn" id="radio1">
+            <input type="radio" name="radio-btn" id="radio2">
+            <input type="radio" name="radio-btn" id="radio3">
+            <input type="radio" name="radio-btn" id="radio4">
 
-        <!-- Full-width images with number and caption text -->
-        <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="/img/Flogin.png" style="width:100%">
-            <div class="text">Caption Text</div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="img2.jpg" style="width:100%">
-            <div class="text">Caption Two</div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="img3.jpg" style="width:100%">
-            <div class="text">Caption Three</div>
-        </div>
-
-        <!-- Next and previous buttons -->
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div>
-    <br>
-
-    <!-- The dots/circles -->
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
-
-
-
-
-
-<div class="games">
-
-<h1 class="slidertitle">Titulo categoria destaque etc...</h1>
-    <div class="slider owl-carousel" style="">
-        <div class="card">
-            <a href="/">
-                <img class="img" src="/img/HDC_logo.svg" alt="Denim Jeans">
-                <div class="content">
-                    <h1 class="title">Titulo do jogo</h1>
-                    <p class="price">R$00,00</p>
-                </div>
-            </a>
-        </div>
-        <div class="card">
-          <a href="/">
-              <img class="img" src="/img/Flogin.png" alt="Denim Jeans">
-              <div class="content">
-                  <h1 class="title">Titulo do jogo2</h1>
-                  <p class="price">R$00,00</p>
-              </div>
-          </a>
-      </div>
-      <div class="card">
-        <a href="/">
-            <img class="img" src="/img/HDC_logo.svg" alt="Denim Jeans">
-            <div class="content">
-                <h1 class="title">Titulo do jogo3</h1>
-                <p class="price">R$00,00</p>
+            <div class="slid first">
+                <img src="/img/Test1.png" alt="">
             </div>
-        </a>
+            <div class="slid">
+                <img src="/img/Test2.png" alt="">
+            </div>
+            <div class="slid">
+                <img src="/img/Test3.png" alt="">
+            </div>
+            <div class="slid">
+                <img src="/img/Test4.png" alt="">
+            </div>
+
+            <div class="navigation-auto">
+                 <div class="auto-btn1"></div>
+                 <div class="auto-btn2"></div>
+                 <div class="auto-btn3"></div>
+                 <div class="auto-btn4"></div>
+
+            </div>
+
+
+
+        </div>
+
+        <div class="navigation-manual">
+          <label for="radio1" class="manual-btn"></label>
+          <label for="radio2" class="manual-btn"></label>
+          <label for="radio3" class="manual-btn"></label>
+          <label for="radio4" class="manual-btn"></label>
+        </div>
     </div>
-    <div class="card">
-      <a href="/">
-          <img class="img" src="/img/HDC_logo.svg" alt="Denim Jeans">
-          <div class="content">
-              <h1 class="title">Titulo do jogo4</h1>
-              <p class="price">R$00,00</p>
-          </div>
-      </a>
-  </div>
+
+<script type="text/javascript">
+var counter =1;
+setInterval(function(){
+document.getElementById('radio'+counter).checked = true;
+counter++;
+if(counter>4){
+  counter=1;
+} 
+}, 5000);
+
+</script>
+
+
+
+
+
+    <div class="games">
+
+        <h1 class="slidertitle">Titulo categoria destaque etc...</h1>
+        <div class="slider owl-carousel" style="">
+            <div class="card">
+                <a href="/">
+                    <img class="img" src="/img/Test1.png" alt="Denim Jeans">
+                    <div class="content">
+                        <h1 class="title">Titulo do jogo</h1>
+                        <p class="price">R$00,00</p>
+                    </div>
+                </a>
+            </div>
+            <div class="card">
+                <a href="/">
+                    <img class="img" src="/img/Test2.png" alt="Denim Jeans">
+                    <div class="content">
+                        <h1 class="title">Titulo do jogo2</h1>
+                        <p class="price">R$00,00</p>
+                    </div>
+                </a>
+            </div>
+            <div class="card">
+                <a href="/">
+                    <img class="img" src="/img/Test3.png" alt="Denim Jeans">
+                    <div class="content">
+                        <h1 class="title">Titulo do jogo3</h1>
+                        <p class="price">R$00,00</p>
+                    </div>
+                </a>
+            </div>
+            <div class="card">
+                <a href="/">
+                    <img class="img" src="/img/Test4.png" alt="Denim Jeans">
+                    <div class="content">
+                        <h1 class="title">Titulo do jogo4</h1>
+                        <p class="price">R$00,00</p>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 
     <script>
         $(".slider").owlCarousel({
             loop: true,
-            margin:20,
+            margin: 20,
             autoplay: true,
             autoplayTimeout: 5000, //1000 = 1 segundo
             autoplayHoverPause: true,
-            
+
         })
     </script>
 
