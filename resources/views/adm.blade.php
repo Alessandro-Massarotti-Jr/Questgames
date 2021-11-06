6bfafadf47eecdd6fea664b811215e12b853cacd
DFelps <daniel-felipe-13@hotmail.com.br>
2021-11-06 19:31:29 -0300

    css adm alterado

--- resources/views/adm.blade.php
+++ resources/views/adm.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADM</title>
     <link rel="stylesheet" href="css/adm.css">
-    
+
 
 </head>
 
@@ -15,9 +15,96 @@
     <div class="div_rect_cima">
         <img class="logo" src="img/logo.png" alt="">
     </div>
-    <div class="div_rect_meio">
-        <p id="p_cadastro">Cadastro de Games</p>
+    <div class="div_dois_retangulo">
+        <div class="div_rect_esquerda">
+            <div class="div_all_bt">
+                <p class="txt_all">Configurações</p>
+                <div class="div_table_bt">
+                    <button class="bt_all" id="bt_games" onclick="MostrarTabela()" style="top: 50px">> Games <
+                            </div>
+                            <div class="div_bt_cadastrar">
+                                <button class="bt_all" id="bt_cadastrar" onclick="Cadastrar()"
+                                    style="top: 90px">CRUD</button>
+                            </div>
+                </div>
+            </div>
+            <div class="div_rect_meio">
+                <p id="p_cadastro">Cadastro de Games</p>
+                <div class="div_tres_quadrados">
+                    <div id="div_cadastro_games" class="div_cadastro_games">
+                        <form action="{{ route('adm/store') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="div_txt">
+                                <p class="all_txt">Nome:</p>
+                                <input type="text" id="inpt_txt_nome" name="inpt_txt_nome"
+                                    class="all_txt">
+                                <p class="all_txt">Preço:</p>
+                                <input type="text" id="inpt_txt_preco" name="inpt_txt_preco"
+                                    class="all_txt">
+                                <p class="all_txt">Descrição:</p>
+                                <input type="text" id="inpt_txt_descr" name="inpt_txt_descr"
+                                    class="all_txt">
+                                <p class="all_txt">Desenvolvedor:</p>
+                                <input type="text" id="inpt_txt_desenvol" name="inpt_txt_desenvol"
+                                    class="all_txt">
+                                <p class="all_txt">Categoria:</p>
+                                <select name="inpt_select_category"
+                                    id="inpt_select_category">
+                                    @foreach ($categorias as $categoria)
+                                        <option value="{{ $categoria->category_name }}">
+                                            {{ $categoria->category_name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                <p class="all_txt">Fundo pag/loja:</p>
+                                <input type="file" name="file_img_fundo" id="file_img_fundo"
+                                    class="file_img file_img_fundo">
+                                <p class="all_txt">Print 1:</p>
+                                <input type="file" name="" id="file_img_print1" class="file_img file_img_print1">
+                                <p class="all_txt">Print 2:</p>
+                                <input type="file" name="" id="file_img_print2" class="file_img file_img_print2">
+                                <p class="all_txt">Print 3:</p>
+                                <input type="file" name="" id="file_img_print3" class="file_img file_img_print3">
+                                <button type="submit" class="bt_margin" id="bt_salvar">Salvar</button>
+                        </form>
+                        <input type="button" class="bt_margin" value="Testar" id="bt_testar" onclick="escrever(this);">
 
+                    </div>
+                </div>
+
+                <!-- PARTE TELINHA SIMULAÇÃO   -->
+                <div id="div_simupag">
+                    <div class="div_imgfundo">
+                        <img id="img_fundo" src="/img/imgadm.png">
+                    </div>
+                    <div class="div_tit_preco">
+                        <p id="p_titulo">Titulo do jogo</p>
+                        <div class="div_rect_compra">
+                            <p id="p_preco2">R$00,00</p>
+                        </div>
+                    </div>
+                    <div class="div_desc_img">
+                        <div class="div_rect_desc">
+                            <p id="p_desc2">Digite uma descrição aqui :)</p>
+                        </div>
+                        <div class="div_3img">
+                            <img id="img_print1" src="/img/imgadm.png">
+                            <img id="img_print2" src="/img/imgadm.png">
+                            <img id="img_print3" src="/img/imgadm.png">
+                        </div>
+                    </div>
+                </div>
+
+                <div id="div_simuloja">
+                    <h1 id="h1_titulo">Titulo do jogo</h1>
+                    <img id="img_fundo2" src="/img/imgadm.png">
+                    <p id="p_preco3">R$00,00</p>
+                </div>
+
+
+
+           
+        </div>
         <div id="div_alterar">
             <p class="p_alterar">Alterar Games</p>
             <select name="ID" id="sel_alterar_id">
@@ -30,113 +117,46 @@
 
             <button class="bt_alt_alterar">Alterar</button>
         </div>
-        
+
         <div id="div_excluir">
             <p class="p_excluir">Excluir Games</p>
-            @foreach($todosjogos as $game)
-            <form action="/adm/{{ $game->nome }}" method="POST">
-                <select name="ID" id="sel_alterar_id">
-                    <option value="opt_nome">{{ $game -> nome }}</option>
-                    @endforeach
-                </select>
-                <input type="hidden" name="_method" value="delete">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="submit" name="nome" class="bt_alt_alterar" value="Excluir">
+            @foreach ($todosjogos as $game)
+                <form action="/adm/{{ $game->nome }}" method="POST">
+                    <select name="ID" id="sel_alterar_id">
+                        <option value="opt_nome">{{ $game->nome }}</option>
+            @endforeach
+            </select>
+            <input type="hidden" name="_method" value="delete">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input type="submit" name="nome" class="bt_alt_alterar" value="Excluir">
             </form>
         </div>
-
-        <!-- PARTE TELINHA SIMULAÇÃO   -->
-        <div id="div_simupag">
-            <div class="div_imgfundo">
-                <img id="img_fundo" src="/img/imgadm.png">
-                <p id="p_titulo">Titulo do jogo</p>
-                <div class="div_rect_compra">
-                    <p id="p_preco2">R$00,00</p>
+        <div class="div_tb_games" id="tb_games">
+            <table>
+                <div class="div_tr">
+                <tr>
+                    <th class="th_td_margin">| ID |</th>
+                    <th class="th_td_margin">| Nome |</th>
+                    <th class="th_td_margin">| Preço |</th>
+                    <th class="th_td_margin">| Descrição |</th>
+                    <th class="th_td_margin">| Desenvolvedor |</th>
+                </tr>
                 </div>
-                <div class="div_rect_desc">
-                    <p id="p_desc2">Digite uma descrição aqui :)</p>
+                @foreach ($todosjogos as $game)
+                <div class="div_tr">
+                    <tr>
+                        <td class="th_td_margin">| {{ $game->id }} |</td>
+                        <td class="th_td_margin">| {{ $game->nome }} |</td>
+                        <td class="th_td_margin">| {{ $game->preco }} |</td>
+                        <td class="th_td_margin">| {{ $game->description }} |</td>
+                        <td class="th_td_margin">| {{ $game->desenvolvedor }} |</td>
+                    </tr>
                 </div>
-                <div class="div_3img">
-                    <img id="img_print1" src="/img/imgadm.png">
-                    <img id="img_print2" src="/img/imgadm.png">
-                    <img id="img_print3" src="/img/imgadm.png">
-                </div>
-            </div>
+                @endforeach
+            </table>
         </div>
 
-        <div id="div_simuloja">
-            <h1 id="h1_titulo">Titulo do jogo</h1>
-            <img id="img_fundo2" src="/img/imgadm.png">
-            <p id="p_preco3">R$00,00</p>
-        </div>
-
-
-
-
-        <table id="tb_games">
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Preço</th>
-                <th>Descrição</th>
-                <th>Desenvolvedor</th>
-            </tr>
-            @foreach($todosjogos as $game)
-            <tr>
-                <td>{{ $game -> id }}</td>
-                <td>{{ $game -> nome }}</td>
-                <td>{{ $game -> preco }}</td>
-                <td>{{ $game -> description }}</td>
-                <td>{{ $game -> desenvolvedor }}</td>
-            </tr>
-            @endforeach
-        </table>
-        
-        <form action="{{route('adm/store')}}" method="post" enctype="multipart/form-data">
-            @csrf
-            <div id="div_cadastro_games" class="div_cadastro_games">
-                <div class="div_txt">
-                    <p class="txt_format txt_nome">Nome:</p>
-                    <input type="text" id="inpt_txt_nome" name="inpt_txt_nome" class="inpt_txt inpt_txt_nome">
-                    <p class="txt_format txt_preco">Preço:</p>
-                    <input type="text" id="inpt_txt_preco" name="inpt_txt_preco" class="inpt_txt inpt_txt_preco">
-                    <p class="txt_format txt_descr">Descrição:</p>
-                    <input type="text" id="inpt_txt_descr" name="inpt_txt_descr" class="inpt_txt inpt_txt_descr">
-                    <p class="txt_format txt_desenvol">Desenvolvedor:</p>
-                    <input type="text" id="inpt_txt_desenvol" name="inpt_txt_desenvol" class="inpt_txt inpt_txt_desenvol">
-                    <p class="txt_format txt_category">Categoria:</p>
-                    <select class="inpt_select_category" name="inpt_select_category" id="inpt_select_category">
-                        @foreach($categorias as $categoria)
-                            <option value="{{$categoria->category_name}}">{{$categoria->category_name}}</option>
-                        @endforeach
-                    </select>
-                    <p class="txt_fundo_pag">Fundo pag/loja:</p>
-                    <input type="file" name="file_img_fundo" id="file_img_fundo" class="file_img file_img_fundo">
-                    <p class="txt_print1">Print 1:</p>
-                    <input type="file" name="" id="file_img_print1" class="file_img file_img_print1">
-                    <p class="txt_print2">Print 2:</p>
-                    <input type="file" name="" id="file_img_print2" class="file_img file_img_print2">
-                    <p class="txt_print3">Print 3:</p>
-                    <input type="file" name="" id="file_img_print3" class="file_img file_img_print3">
-                    <button type="submit" id="bt_salvar">Salvar</button>
-                    
-        </form>
-        <input type="button" value="Testar" id="bt_testar" onclick="escrever(this);">
-       
     </div>
-    </div>
-
-    </div>
-    <div class="div_rect_esquerda">
-        <div class="div_all_bt">
-            <p class="txt_all">Configurações</p>
-            <div class="div_table_bt">
-                <button class="bt_all" id="bt_games" onclick="MostrarTabela()" style="top: 50px">> Games <</button>
-            </div>
-            <div class="div_bt_cadastrar">
-                <button class="bt_all" id="bt_cadastrar" onclick="Cadastrar()" style="top: 90px">CRUD</button>
-            </div>
-        </div>
     </div>
     <script type="text/javascript">
         //CONFIG_GLOBAL
@@ -165,7 +185,7 @@
                 excluir.style.visibility = 'hidden';
                 alterar.style.visibility = 'hidden';
                 cadastro.style.visibility = 'hidden';
-               
+
                 let el = document.getElementById('bt_games');
             }
         }
@@ -188,7 +208,7 @@
                 excluir.style.visibility = 'visible';
                 alterar.style.visibility = 'visible';
                 cadastro.style.visibility = 'visible';
-               
+
                 gam.style.visibility = 'hidden';
                 let el = document.getElementById('bt_cadastrar');
             }
