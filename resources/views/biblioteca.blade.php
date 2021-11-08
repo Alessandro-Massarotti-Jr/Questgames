30665f6f99b33139e34b9e6de43eb9bd093c7886
DFelps <daniel-felipe-13@hotmail.com.br>
2021-11-07 21:14:01 -0300

    biblioteca e side bar alterados

--- resources/views/biblioteca.blade.php
+++ resources/views/biblioteca.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.main')
+@section('title', 'biblioteca')
+@section('content')
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +12,33 @@
     <link rel="stylesheet" href="css/biblio.css">
 </head>
 <body>
+<div class="div_biblio_geral">
+<div class="div_cima_jg">
+     <input type="text" placeholder="Procurar">
+     <ion-icon class="icone" name="search-outline"></ion-icon>
+</div>
+<div class="div_games_recent">
+    <div class="div_recent">
+        <p class="txt_color p_recent">Jogados Recentemente</p>
+    </div>
+    <div class="div_jogo">
+        <img class="img_jogo" src="img/Test4.png" alt="">
+        <p class="txt_color p_titulo_game">Hellen Action Shooter</p>
+       <a class="a_baixar" href="Download">Download</a>
+    </div>
+</div>
 
+<div class="div_games_all">
+    <div class="div_all">
+        <p class="txt_color p_all">Todos os jogos</p>
+    </div>
+    <div class="div_jogo">
+        <img class="img_jogo" src="img/Test4.png" alt="">
+        <p class="txt_color p_titulo_game">Hellen Action Shooter</p>
+       <a class="a_baixar" href="Download">Download</a>
+    </div>
+</div>
+</div>
 </body>
 </html>
+@endsection
