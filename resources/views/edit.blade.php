90c8903f8048fd54dc51d8172b6c4f1c8720c4c5
DFelps <daniel-felipe-13@hotmail.com.br>
2021-11-24 21:53:42 -0300

    mudanca editar perfil

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -11,7 +11,7 @@
     <div class="geral">
         <div class="perfil">
             <div><img class="foto_edit" src="../img/default.png" alt="Alterar">
-                <input type="file" name="file_foto" class="file_foto">
+                <input type="file" name="file_foto" id="file_foto">
             </div>
             <div class="conteudo">
                 <input type="text" class="cx_nick" name="cx_nick">
@@ -28,13 +28,22 @@
 
         <div class="print-sec">
             <div class="print">
-               <div class="print-img"><img src="/img/default_publi.png" alt=""></div>
-               <div class="print-img"><img src="/img/default_publi.png" alt=""></div>
-               
+               <div class="print-img"><img src="/img/default_publi.png" alt="">
+                <input type="file" id="file_publi" name="filepubli1">
+                </div>
+               <div class="print-img"><img src="/img/default_publi.png" alt="">
+                <input type="file" id="file_publi2" name="filepubli2">
             </div>
+                
+                </div>
             <div class="print">
-                <div class="print-img"><img src="/img/default_publi.png" alt=""></div>
-                <div class="print-img"><img src="/img/default_publi.png" alt=""></div>
+                <div class="print-img"><img src="/img/default_publi.png" alt="">
+                    <input type="file" id="file_publi3" name="filepubli3">
+                </div>
+                
+                <div class="print-img"><img src="/img/default_publi.png" alt="">
+                    <input type="file" id="file_publi4" name="filepubli4">
+                </div>
             </div>
         </div>
     </div>
